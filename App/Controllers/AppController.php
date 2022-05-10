f787da0af1ba5d52226986fad498187c4133e218
Joelson Fernandes <fernandesjoelson97@gmail.com>
2022-05-10 15:17:55 -0300

    add sistema de paginação

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -15,9 +15,20 @@
                 $tweet = Container::getModel('Tweet');
                 $tweet->__set('id_usuario', $_SESSION['id']);
 
-                $tweets = $tweet->getAll();
-                $this->view->tweets = $tweets;
+                //variaveis de páginação
+                $total_registros_pagina = 10;
+                //$deslocamento = 0;
+                $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+                $deslocamento = ($pagina - 1) * $total_registros_pagina;
 
+                //$tweets = $tweet->getAll();
+                $tweets = $tweet->getPorPagina($total_registros_pagina, $deslocamento);
+                $total_tweets = $tweet->getTotalRegistros();
+
+                $this->view->total_de_paginas = ceil($total_tweets['total'] / $total_registros_pagina);
+
+                $this->view->tweets = $tweets;
+                $this->view->pagina_ativa = $pagina;
 
                 $this->render('timeline');
           
